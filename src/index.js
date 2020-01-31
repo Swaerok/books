e7cf21cf7939173204744572dfa91a441476e5d0
Swaerok <swaer19950501@gmail.com>
2020-01-31 16:58:13 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-function trye(){
-    return 45
-}
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.scss'
+import App from './components/app'
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root'),
+)
